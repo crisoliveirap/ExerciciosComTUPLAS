cdd8147ac0262e30fa3c2417e90c3d1375b864ec
Cristiane Oliveira <cristiane.passos@hotmail.com>
2022-08-15 11:45:21 -0300

    Initial commit

---
+++ Ex 01.py
@@ -0,0 +1,12 @@
+# Números por extenso
+numeros = ('zero', 'um', 'dois', 'três', 'quatro',
+           'cinco', 'seis', 'sete', 'oito', 'nove',
+           'dez', 'onze', 'doze', 'treze', 'quatorze',
+           'quinze', 'dezesseis', 'dezessete', 'dezoito',
+           'dezenove', 'vinte')
+while True:
+    pergunta = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= pergunta <= 20:
+        break
+    print('Tente novamente.', end='')
+print(f'Você digitou o número {numeros[pergunta]}.')
---
+++ Ex 04.py
@@ -0,0 +1,16 @@
+# Analisando dados em uma tupla
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+c = int(input('Digite mais um número: '))
+d = int(input('Digite o último número: '))
+numeros = (a, b, c, d)
+print(f'Você digitou os valores: {numeros})')
+print(f'O valor 9 apareceu {numeros.count(9)} vezes.')
+if 3 in numeros:
+    print(f'O valor 3 apareceu na {numeros.index(3)+1}ª posição.')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição.')
+print(f'Os valores pares digitados foram: ', end='')
+for n in numeros:
+    if n % 2 == 0:
+        print(n, end=' ')
